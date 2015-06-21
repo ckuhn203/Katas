c4b5780f698637c6618fbf79c9cf8ae821a2644a
Christopher J. McClellan <ckuhn203@gmail.com>
2015-06-21 02:08:31 -0400

    abstracted a more general BuyXGetOneStrategy from Bogo

--- SuperMarketPricing/BogoStrategy.cs
+++ SuperMarketPricing/BogoStrategy.cs
@@ -2,18 +2,32 @@
 
 namespace SuperMarketPricing
 {
-    public class BogoStrategy : IPricingStrategy
+    public class BogoStrategy : BuyXGetOneStrategy
+    {
+        public BogoStrategy(double price)
+            : base(price, 2)
+        { }
+    }
+
+    public class BuyXGetOneStrategy : IPricingStrategy
     {
         private double _price;
+        private int _xToBuy;
 
-        public BogoStrategy(double price)
+        public BuyXGetOneStrategy(double price, int xToBuy)
         {
             if (price < 0)
             {
                 throw new ArgumentOutOfRangeException(nameof(price), price, "Cannot be less than zero.");
             }
 
+            if (xToBuy < 2)
+            {
+                throw new ArgumentOutOfRangeException(nameof(xToBuy), xToBuy, "Cannot be less than two.");
+            }
+
             _price = price;
+            _xToBuy = xToBuy;
         }
 
         public double GetPrice(int count)
@@ -24,11 +38,11 @@ namespace SuperMarketPricing
             }
 
             double result = 0;
-            
-            while (count - 2 > 0)
+
+            while (count - _xToBuy > 0)
             {
                 result = result + _price;
-                count = count - 2;
+                count = count - _xToBuy;
             }
 
             if (count > 0)
