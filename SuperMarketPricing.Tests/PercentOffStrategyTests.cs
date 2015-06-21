0f99459e6e55277dba6cf6072d6ad6801904a37f
Christopher J. McClellan <ckuhn203@gmail.com>
2015-06-21 00:53:28 -0400

    Added bounds checking to PercentOffStrategy

---
+++ SuperMarketPricing.Tests/PercentOffStrategyTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SuperMarketPricing.Tests
+{
+    [TestClass]
+    public class PercentOffStrategyTests
+    {
+        private readonly Sku sku = new Sku('A');
+
+        [TestMethod]
+        public void PercentOff_SingleItem()
+        {
+            var strat = new PercentOffStrategy(sku, 100, 10);
+
+            var price = strat.GetPrice(1);
+
+            Assert.AreEqual(90, price);
+        }
+
+        [TestMethod]
+        public void PercentOff_MultipleItems()
+        {
+            var strat = new PercentOffStrategy(sku, 2.50, 10);
+
+            var price = strat.GetPrice(2);
+
+            Assert.AreEqual(4.50, price);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PercentOff_WhenPercentageIsEqualToZero_ThrowsArgOutOfRangeException()
+        {
+            var strat = new PercentOffStrategy(sku, 100, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PercentOff_WhenPercentageIsLessThanZero_ThrowsArgOutOfRangeException()
+        {
+            var strat = new PercentOffStrategy(sku, 100, -1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PercentOff_WhenPercentageIsGreaterThanEqualToOneHundred_ThrowsOutOfRangeException()
+        {
+            var strat = new PercentOffStrategy(sku, 100, 100);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PercentOff_WhenPriceIsNegative_ThrowsOutOfRangeException()
+        {
+            var strat = new PercentOffStrategy(sku, -20, 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PercentOff_GetPrice_WhenCountIsNegative_ThrowsOutOfRangeException()
+        {
+            var strat = new PercentOffStrategy(sku, 100, 10);
+
+            var price = strat.GetPrice(-5);
+        }
+
+    }
+}
--- SuperMarketPricing/PercentOffStrategy.cs
+++ SuperMarketPricing/PercentOffStrategy.cs
@@ -1,4 +1,6 @@
-﻿namespace SuperMarketPricing
+﻿using System;
+
+namespace SuperMarketPricing
 {
     public class PercentOffStrategy : IPricingStrategy
     {
@@ -9,13 +11,34 @@
 
         public PercentOffStrategy(Sku sku, double price, int percentOff)
         {
-            Sku = sku;
-            _price = price;
+            if (percentOff <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(percentOff), percentOff, "Cannot be less than or equal to zero.");
+            }
+
+            if (percentOff >= 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(percentOff), percentOff, "Cannot be equal to or greater than 100.");
+            }
+
+            if (price < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(price), price, "Cannot be less than zero.");
+            }
+
             _percentOff = percentOff;
+            _price = price;
+   
+            Sku = sku;
         }
 
         public double GetPrice(int count)
         {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), count, "Cannot be less than zero.");
+            }
+
             return (_price - (_price / _percentOff)) * count;
         }
     }
