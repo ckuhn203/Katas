60ecb73b44c0eafff6e1e456f17e81c3f3aa48d9
Christopher J. McClellan <ckuhn203@gmail.com>
2015-06-21 01:08:57 -0400

    doc comments

--- SuperMarketPricing/PercentOffStrategy.cs
+++ SuperMarketPricing/PercentOffStrategy.cs
@@ -2,6 +2,9 @@
 
 namespace SuperMarketPricing
 {
+    /// <summary>
+    /// Uses a "X% Off" Pricing Strategy.
+    /// </summary>
     public class PercentOffStrategy : IPricingStrategy
     {
         private int _percentOff;
@@ -9,6 +12,9 @@ namespace SuperMarketPricing
 
         public Sku Sku { get; }
 
+        /// <param name="sku">The <see cref="Sku"/> to that uses this strategy.</param>
+        /// <param name="price">The regular every day price.</param>
+        /// <param name="percentOff">Whole number from 1 to 99.</param>
         public PercentOffStrategy(Sku sku, double price, int percentOff)
         {
             if (percentOff <= 0)
--- SuperMarketPricing/RegularStrategy.cs
+++ SuperMarketPricing/RegularStrategy.cs
@@ -1,4 +1,6 @@
-﻿namespace SuperMarketPricing
+﻿using System;
+
+namespace SuperMarketPricing
 {
     /// <summary>
     /// Everyday pricing strategy for non-sale items.
@@ -11,12 +13,22 @@
 
         public RegularStrategy(Sku sku, double price)
         {
-            Sku = sku;
+            if (price < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(price), price, "Cannot be less than zero.");
+            }
+
             _price = price;
+            Sku = sku;
         }
 
         public double GetPrice(int count)
         {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), count, "Cannot be less than zero.");
+            }
+
             return _price * count;
         }
     }
