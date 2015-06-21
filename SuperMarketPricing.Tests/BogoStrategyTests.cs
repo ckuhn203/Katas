2690d8df86d517fc2b2291c44b6ce728bc3140cf
Christopher J. McClellan <ckuhn203@gmail.com>
2015-06-21 02:00:22 -0400

    Added bogo strategy

---
+++ SuperMarketPricing.Tests/BogoStrategyTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SuperMarketPricing.Tests
+{
+    [TestClass]
+    public class BogoStrategyTests
+    {
+        [TestMethod]
+        public void Bogo_WhenZeroItems_PriceIsZero()
+        {
+            var strat = new BogoStrategy(2.50);
+            var price = strat.GetPrice(0);
+
+            Assert.AreEqual(0, price);
+        }
+
+        [TestMethod]
+        public void Bogo_WhenOneItem_PriceIsFull()
+        {
+            var strat = new BogoStrategy(2.50);
+            var price = strat.GetPrice(1);
+
+            Assert.AreEqual(2.50, price);
+        }
+
+        [TestMethod]
+        public void Bogo_WhenTwoItems_PriceIsHalf()
+        {
+            var strat = new BogoStrategy(2.50);
+            var price = strat.GetPrice(2);
+
+            Assert.AreEqual(2.50, price);
+        }
+
+        [TestMethod]
+        public void Bogo_WhenThreeItems_PriceIsHalfPlusFull()
+        {
+            var strat = new BogoStrategy(2.50);
+            var price = strat.GetPrice(3);
+
+            Assert.AreEqual(5.00, price);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Bogo_WhenItemCountIsNegative_ThrowsArgumentOutOfRangeException()
+        {
+            var strat = new BogoStrategy(2.50);
+            var price = strat.GetPrice(-5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Bogo_WhenPriceIsNegative_ThrowsArgumentOutOfRangeException()
+        {
+            var strat = new BogoStrategy(-2.50);
+        }
+    }
+}
---
+++ SuperMarketPricing/BogoStrategy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SuperMarketPricing
+{
+    public class BogoStrategy : IPricingStrategy
+    {
+        private double _price;
+
+        public BogoStrategy(double price)
+        {
+            if (price < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(price), price, "Cannot be less than zero.");
+            }
+
+            _price = price;
+        }
+
+        public double GetPrice(int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), count, "Cannot be less than zero.");
+            }
+
+            double result = 0;
+            
+            while (count - 2 > 0)
+            {
+                result = result + _price;
+                count = count - 2;
+            }
+
+            if (count > 0)
+            {
+                result = result + _price;
+            }
+
+            return result;
+        }
+    }
+}
