1caa9483f09debe391a2275d5565cd2e99891a1b
Christopher McClellan <ckuhn203@gmail.com>
2015-11-27 16:38:54 -0500

    Implemented IPv4 Address struct.

--- Rubberduck.Katas.Network/Rubberduck.Katas.Network.Tests/Ip4AddressTests.cs
+++ Rubberduck.Katas.Network/Rubberduck.Katas.Network.Tests/Ip4AddressTests.cs
@@ -7,8 +7,59 @@ namespace Rubberduck.Katas.Network.Tests
     public class Ip4AddressTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CanCreateFromString()
         {
+            Ip4Address ip = new Ip4Address("192.10.1.1");
+
+            Assert.AreEqual(192, ip.Octet1);
+            Assert.AreEqual(10, ip.Octet2);
+            Assert.AreEqual(1, ip.Octet3);
+            Assert.AreEqual(1, ip.Octet4);
+        }
+
+        [TestMethod]
+        public void CanCreateFromByteArray()
+        {
+            Ip4Address ip = new Ip4Address(new byte[] {192, 10, 1, 1});
+
+            Assert.AreEqual(192, ip.Octet1);
+            Assert.AreEqual(10, ip.Octet2);
+            Assert.AreEqual(1, ip.Octet3);
+            Assert.AreEqual(1, ip.Octet4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ByteArrayLengthCannotBeLessThan4()
+        {
+            var ip = new Ip4Address(new byte[] {});
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ByteArrayLengthCannotBeGreaterThan4()
+        {
+            var ip = new Ip4Address(new byte[] {1, 1, 1, 1, 1});
+        }
+
+        [TestMethod]
+        public void CanCreateFromBaseTenAddress()
+        {
+            //i.e. can create from an integer.
+            var ip = new Ip4Address(UInt32.MaxValue); //0xFFFFFFFF
+
+            Assert.AreEqual(255, ip.Octet1);
+            Assert.AreEqual(255, ip.Octet2);
+            Assert.AreEqual(255, ip.Octet3);
+            Assert.AreEqual(255, ip.Octet4);
+        }
+
+        [TestMethod]
+        public void ToStringReturnsExpectedResult()
+        {
+            var ip = new Ip4Address(new byte[] {10, 10, 1, 1});
+
+            Assert.AreEqual("10.10.1.1", ip.ToString());
         }
     }
 }
--- Rubberduck.Katas.Network/Rubberduck.Katas.Network/Ip4Address.cs
+++ Rubberduck.Katas.Network/Rubberduck.Katas.Network/Ip4Address.cs
@@ -1,12 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace Rubberduck.Katas.Network
 {
+    [StructLayout(LayoutKind.Explicit)]
     public struct Ip4Address
     {
+        [FieldOffset(0)] public byte Octet1;
+        [FieldOffset(1)] public byte Octet2;
+        [FieldOffset(2)] public byte Octet3;
+        [FieldOffset(3)] public byte Octet4;
+
+        /// <summary>
+        /// Represents the Base Ten IPv4 address.
+        /// </summary>
+        /// <remarks>Overlays the Octet fields, so changing this value changes the Octets & vice versa.</remarks>
+        // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
+        // ReSharper disable once FieldCanBeMadeReadOnly.Local
+        [FieldOffset(0)] private UInt32 Address;
+
+        /// <summary>
+        /// Must be a valid IPv4 address.
+        /// </summary>
+        /// <param name="address"></param>
+        public Ip4Address(string address)
+            :this(address.Split('.').Select(a => Byte.Parse(a)).ToArray())
+        { }
+
+        /// <summary>
+        /// Creates a new Ip4Address from a byte array.
+        /// </summary>
+        /// <param name="address">
+        /// Must be an array of Length 4. 
+        /// Index 0 is mapped to the first octet.
+        /// </param>
+        public Ip4Address(byte[] address)
+            :this()
+        {
+            const int expectedLength = 4;
+
+            if (address.Length != expectedLength)
+            {
+                throw new ArgumentException($"{nameof(address)} array must have a length of {expectedLength}.", nameof(address));
+            }
+
+            Octet1 = address[0];
+            Octet2 = address[1];
+            Octet3 = address[2];
+            Octet4 = address[3];
+        }
+
+        /// <summary>
+        /// Creates a new Ip4Address from it's base ten representation.
+        /// </summary>
+        /// <param name="address">Base ten representation of an IPv4 address. 
+        /// UInt32.MaxValue results in an IP of "255.255.255.255".
+        /// </param>
+        public Ip4Address(UInt32 address)
+            :this()
+        {
+            Address = address;
+        }
+
+        public override string ToString()
+        {
+            return $"{Octet1}.{Octet2}.{Octet3}.{Octet4}";
+        }
     }
 }
