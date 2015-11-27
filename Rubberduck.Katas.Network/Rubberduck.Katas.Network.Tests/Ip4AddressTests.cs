4bd4db57a2b4f0e546d7cd67f9a751460f306f8d
Christopher McClellan <ckuhn203@gmail.com>
2015-11-27 15:12:15 -0500

    Initial commit for Ip4Address/Explicitly laid out struct project

---
+++ Rubberduck.Katas.Network/Rubberduck.Katas.Network.Tests/Ip4AddressTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rubberduck.Katas.Network.Tests
+{
+    [TestClass]
+    public class Ip4AddressTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
